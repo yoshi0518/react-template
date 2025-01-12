1cee01e8018e0cc94b38d6ffcda2aa49ac419de6
yoshi0518 <yoshihumi.n.0518@gmail.com>
2025-01-12 23:48:04 +0900

    shadcn/uiを追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
+import { Button } from "./components/ui/button";
+
 function App() {
   return (
     <>
       <h1 className="text-center text-semibold text-2xl">React Template</h1>
+      <div className="flex justify-center">
+        <Button>Button</Button>
+      </div>
     </>
   );
 }
